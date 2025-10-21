80d3d975efed483cbc5bcb290604eafc43c71ebb
Christian Pircher <mail@christian-pircher.com>
2025-10-21 19:23:51 +0000

    chore(csp): TEMP allow framing from any domain for WP test

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          // TEMP: erlaubt Einbettung aus allen Domains
+          { key: 'Content-Security-Policy', value: "frame-ancestors 'self' *;" },
+        ],
+      },
+    ];
+  },
+};
 export default nextConfig;
