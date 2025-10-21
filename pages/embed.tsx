f98658d153a6af6e34f991f4ea8fb961f3854afd
Christian Pircher <mail@christian-pircher.com>
2025-10-21 18:58:25 +0000

    fix: remove pages/ duplicates for /embed and /testalive

--- pages/embed.tsx
+++
@@ -1,7 +0,0 @@
-export default function EmbedAlivePages() {
-  return (
-    <div style={{ fontFamily: "ui-sans-serif, system-ui", padding: 16 }}>
-      ✅ /embed alive (pages router)
-    </div>
-  );
-}
--- pages/testalive.tsx
+++
@@ -1,3 +0,0 @@
-export default function TestAlivePages() {
-  return <div style={{padding:16,fontFamily:"ui-sans-serif"}}>🟢 /testalive (pages router) visible</div>;
-}
